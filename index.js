12cb31c6602b6b7bc66ad1674808904ecc8de9db
Winnie-Flora <contentcreatorwinnie@gmail.com>
2024-06-27 14:31:15 +0300

    Adds javascript functions continued

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+function saturdayFun(activity="roller-skate"){
+    return`This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+
+
+function wrapAdjective(exc="*"){
+    const innerFunction = function(adj="special"){
+    return `You are ${exc}${adj}${exc}!`;
+    };
+    return innerFunction;
+}
+wrapAdjective("%")("a dedicated programmer");
+
+
+
+
+
+
